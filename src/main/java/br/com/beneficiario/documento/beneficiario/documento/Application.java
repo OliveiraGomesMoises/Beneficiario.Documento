8665ac01dc920a992373fca573c73852216fdf78
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-09-05 13:37:23 -0300

    implementa api teste

--- src/main/java/br/com/beneficiario/documento/beneficiario/documento/Application.java
+++ src/main/java/br/com/beneficiario/documento/beneficiario/documento/Application.java
@@ -2,12 +2,18 @@ package br.com.beneficiario.documento.beneficiario.documento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/")
 public class Application {
-
+	@GetMapping
+	public String getHometeste(){
+		return "beneficiario documento -API HOME";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
