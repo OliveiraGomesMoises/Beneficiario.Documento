5e596387c92137ebd746b87df7e276d6c18ff955
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-09-05 12:25:43 -0300

    inicia projeto

---
+++ src/main/java/br/com/beneficiario/documento/beneficiario/documento/Application.java
@@ -0,0 +1,17 @@
+package br.com.beneficiario.documento.beneficiario.documento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public String
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
