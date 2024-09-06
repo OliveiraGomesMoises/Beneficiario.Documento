37cf9b0eed8f320dbf0a2c0d3e41ee682b5f7be5
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-09-06 10:36:32 -0300

    implementa handler e tratamento de erro no infraRepository

--- src/main/java/br/com/beneficiario/documento/beneficiario/documento/beneficiario/application/repository/BeneficiarioInfraRepository.java
+++ src/main/java/br/com/beneficiario/documento/beneficiario/documento/beneficiario/application/repository/BeneficiarioInfraRepository.java
@@ -2,20 +2,27 @@ package br.com.beneficiario.documento.beneficiario.documento.beneficiario.applic
 
 import br.com.beneficiario.documento.beneficiario.documento.beneficiario.domain.Beneficiario;
 import br.com.beneficiario.documento.beneficiario.documento.beneficiario.infra.BeneficiarioSpringDataJPARepository;
+import br.com.beneficiario.documento.beneficiario.documento.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Log4j2
 @RequiredArgsConstructor
-public class BeneficiarioInfraRepository implements BeneficiarioRepository{
+public class BeneficiarioInfraRepository implements BeneficiarioRepository {
     private final BeneficiarioSpringDataJPARepository beneficiarioSpringDataJPARepository;
 
     @Override
     public Beneficiario salva(Beneficiario beneficiario) {
         log.info("[inicia] BeneficiarioInfraRepository - salva");
-        beneficiarioSpringDataJPARepository.save(beneficiario);
+        try {
+            beneficiarioSpringDataJPARepository.save(beneficiario);
+        } catch (DataIntegrityViolationException e) {
+            throw APIException.build(HttpStatus.BAD_REQUEST, "Existe dados duolicados!", e);
+        }
         log.info("[finaliza] BeneficiarioInfraRepository - salva");
         return beneficiario;
     }
