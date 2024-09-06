94a474c43254b83f0ba4888c36cab854e32f7cfa
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-09-06 10:15:06 -0300

    implementa springDataRepository teste ok!

--- src/main/java/br/com/beneficiario/documento/beneficiario/documento/beneficiario/application/repository/BeneficiarioInfraRepository.java
+++ src/main/java/br/com/beneficiario/documento/beneficiario/documento/beneficiario/application/repository/BeneficiarioInfraRepository.java
@@ -1,16 +1,21 @@
 package br.com.beneficiario.documento.beneficiario.documento.beneficiario.application.repository;
 
 import br.com.beneficiario.documento.beneficiario.documento.beneficiario.domain.Beneficiario;
+import br.com.beneficiario.documento.beneficiario.documento.beneficiario.infra.BeneficiarioSpringDataJPARepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Log4j2
-
+@RequiredArgsConstructor
 public class BeneficiarioInfraRepository implements BeneficiarioRepository{
+    private final BeneficiarioSpringDataJPARepository beneficiarioSpringDataJPARepository;
+
     @Override
     public Beneficiario salva(Beneficiario beneficiario) {
         log.info("[inicia] BeneficiarioInfraRepository - salva");
+        beneficiarioSpringDataJPARepository.save(beneficiario);
         log.info("[finaliza] BeneficiarioInfraRepository - salva");
         return beneficiario;
     }
---
+++ src/main/java/br/com/beneficiario/documento/beneficiario/documento/beneficiario/infra/BeneficiarioSpringDataJPARepository.java
@@ -0,0 +1,9 @@
+package br.com.beneficiario.documento.beneficiario.documento.beneficiario.infra;
+
+import br.com.beneficiario.documento.beneficiario.documento.beneficiario.domain.Beneficiario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeneficiarioSpringDataJPARepository extends JpaRepository<Beneficiario, UUID> {
+}
